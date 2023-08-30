6178454d92a95eb7de14923623db4aa3478a1b97
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-08-30 14:24:29 -0400

    incorporating regx

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const equation = 2 + 4 * 3 - 7
+// const equation = 2 + 44* 3 - 7 /8
 
 // 2 + 12 - 7 = parse(2 + 4 * 3 - 7)
 // 14- 7 = parse(2 + 12 - 7)
@@ -15,7 +15,42 @@ solveButton.addEventListener("click", (e) => {
 })
 
 function parse(equation) {
-	const step = getNextStep()
-	const result = solve(step)
-	replaceNextStep(equation, step, result)
+	const step = getNextStep(equation)
+	console.log(step)
+	// const result = solve(step)
+	// replaceNextStep(equation, step, result)
 }
+
+// const regx = /\d+/
+// const str = "there are 123 numbers"
+
+// const [matches] = str.match(regx)
+// console.log(matches)
+// console.log(str.includes('there'))
+
+function getNextStep(equation) {
+	if (equation.includes("*") || equation.includes("/")) {
+		const regx = /(\d+)\s*\*\s*(\d+)/g // get #s L and R of *
+		const matches = []
+		let match
+        // console.log(regx.exec(equation))
+
+		while ((match = regx.exec(equation)) !== null) {
+            const leftNum = match[1]
+			const [operand] = equation.match(/\*/)
+			const rightNum = match[2]
+			matches.push({ leftNum, operand, rightNum })
+		}
+		return matches
+	}
+}
+
+
+// 2 + 44* 3 - 7 /8
+
+// function getRegx(...args){
+//     for(const arg of args){
+
+//     }
+
+// }
