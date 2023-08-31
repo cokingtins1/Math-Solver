767461793fa9b3a0cbbc131ffa4a38175f39ec0b
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-08-31 10:11:29 -0400

    update

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ function getNextStep(equation) {
 		const regx = /(\d+)\s*\*\s*(\d+)/g // get #s L and R of *
 		const matches = []
 		let match
-        // console.log(regx.exec(equation))
+		// console.log(regx.exec(equation))
 
 		while ((match = regx.exec(equation)) !== null) {
-            const leftNum = match[1]
+			const leftNum = match[1]
 			const [operand] = equation.match(/\*/)
 			const rightNum = match[2]
 			matches.push({ leftNum, operand, rightNum })
@@ -45,12 +45,55 @@ function getNextStep(equation) {
 	}
 }
 
+const testEq = "2 + 44* 3 - 7 /8+5^2 + 10+8"
+// "(2 + 44)* 3 - 7 /8 + 5^2 + 10 + 8"
+// "((2 + 44)* 3) - (7/8)+(5^2) + (10+8)"
+// 
+const groups = addParentheses(testEq)
+console.log(groups)
 
-// 2 + 44* 3 - 7 /8
+function addParentheses(equation) {
+	equation = equation.replace(/\s+/g, "")
+	const regx = /(\d+)([\*/^])(\d+)/g
+	const matches = []
+	let match
 
-// function getRegx(...args){
-//     for(const arg of args){
+	while ((match = regx.exec(equation)) !== null) {
+		const subEq = {
+			group: match[0],
+			left: Number(match[1]),
+			operand: match[2],
+			right: Number(match[3]),
+		}
 
-//     }
+		matches.push(subEq)
+		doOperation(matches)
+		
+	}
+	return matches
+}
 
-// }
+function doOperation(equation) {
+	equation.forEach((group) => {
+		switch (group.operand) {
+			case "+":
+				group.group = group.left + group.right
+				break
+			case "-":
+				group.group = group.left - group.right
+				break
+			case "*":
+				group.group = group.left * group.right
+				break
+			case "/":
+				group.group = group.left / group.right
+				break
+			case "^":
+				group.group = group.left ** group.right
+				break
+
+			default:
+				return
+		}
+	})
+}
